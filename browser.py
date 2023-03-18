6c9b96fbb38ee116b1a3fb947125d8ee27d9d3a9
Vishwet Nadimetla <103368238+mr-vishwet@users.noreply.github.com>
2023-03-19 04:45:21 +0530

    Update browser.py

--- browser.py
+++ browser.py
@@ -1,31 +1,57 @@
 import streamlit as st
 import requests
-from io import BytesIO
+import os
 import base64
 
+
 # Define GitHub repository URL and PDF file name
-REPO_URL = "https://github.com/mr-nani/Demo"
+REPO_URL = "https://github.com/mr-vishwet/Demo"
 PDF_FILE = "blockchain.pdf"
+PDF_PATH = os.path.join(os.getcwd(), PDF_FILE)
 
 # Define function to download PDF file from GitHub repository
+
+
 def download_pdf():
     pdf_url = f"{REPO_URL}/blob/main/{PDF_FILE}?raw=true"
+    st.write(PDF_PATH)
     response = requests.get(pdf_url)
-    pdf_data = BytesIO(response.content)
-    return pdf_data
+    pdf_data = response.content
+    with open(PDF_PATH, "wb") as f:
+        f.write(pdf_data)
+
+
+def pdf_viewer(pdf_bytes):
+    data_url = f"data:application/pdf;base64,{base64.b64encode(pdf_bytes).decode()}"
+    st.markdown(
+        f'<iframe src="{data_url}" width="800" height="1200" style="border: none;"></iframe>', unsafe_allow_html=True,)
+
 
 # Define Streamlit app
+
+
 def main():
     st.title("PDF Viewer")
 
     # Download PDF file from GitHub repository
-    pdf_data = download_pdf()
+    download_pdf()
+    st.write(PDF_PATH)
 
-    # Display PDF file contents
-    pdf_base64 = base64.b64encode(pdf_data.read()).decode("utf-8")
-    pdf_display = f'<embed src="data:application/pdf;base64,{pdf_base64}" width="100%" height="600px"/>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+    # Display PDF file contents as a PDF viewer
+    if os.path.isfile(PDF_PATH):
+
+        with open(PDF_PATH, "rb") as f:
+            pdf_bytes = f.read()
+
+        if pdf_bytes:
+            data_url = f"data:application/pdf;base64,{base64.b64encode(pdf_bytes).decode()}"
+            # st.write("Data_url : ", data_url)
+            st.markdown(
+                f'<iframe src="{data_url}" width="700" height="1000" style="border: none"></iframe>', unsafe_allow_html=True)
+
+        else:
+            st.write("Error while reading the file")
+
 
 if __name__ == "__main__":
     main()
-
